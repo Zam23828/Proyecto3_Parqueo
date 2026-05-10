9d2d07f3632bf7127e25cf64e57e7ee460783457
Zam23828 <zam23828@uvg.edu.gt>
2026-05-09 23:28:25 -0600

    I2C funcional
    Funciona la página web con los sensores.

--- STM_7seg/STM_7seg/Core/Src/main.c
+++ STM_7seg/STM_7seg/Core/Src/main.c
@@ -43,6 +43,8 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+I2C_HandleTypeDef hi2c1;
+
 TIM_HandleTypeDef htim1;
 DMA_HandleTypeDef hdma_tim1_ch1;
 
@@ -51,6 +53,10 @@ UART_HandleTypeDef huart2;
 /* USER CODE BEGIN PV */
 uint8_t lecturas_sensores[4];
 float brilloled = 50.0f;
+
+// Buffers para I2C
+uint8_t aTxBuffer[4] = {0, 0, 0, 0}; // Estado de los 4 parqueos (0=Libre, 1=Ocupado)
+uint8_t aRxBuffer[1] = {0};          // Para recibir el comando 'S' del ESP32
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -60,6 +66,7 @@ static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -75,7 +82,6 @@ static void MX_TIM1_Init(void);
   */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -102,11 +108,17 @@ int main(void)
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   MX_TIM1_Init();
+  MX_I2C1_Init();
+
   /* USER CODE BEGIN 2 */
   HAL_ADC_Start_DMA(&hadc1, (uint32_t*)lecturas_sensores, 4);
   setBrightness(25);
   pixelClear();
   pixelShow();
+
+  // Iniciar a escuchar peticiones I2C del ESP32 maestro sin detenerse si hay un fallo inicial
+  HAL_I2C_EnableListen_IT(&hi2c1);
+
   HAL_Delay(1000);
   /* USER CODE END 2 */
 
@@ -117,20 +129,23 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)lecturas_sensores, 4);
-	  HAL_Delay(20);                    // wait for ~4 conversions to finish (~5µs needed, 20ms plenty)
-	  HAL_ADC_Stop_DMA(&hadc1);
-	  for(int i = 0; i < 4; i++) {
-			if(lecturas_sensores[i] < 230) {
-				// Valor menor a 230 -> Color VERDE
-				setPixelColor(i, 0, 255, 0);
-			} else {
-				// Valor mayor o igual a 230 -> Color ROJO
-				setPixelColor(i, 255, 0, 0);
-			}
-		}
-	  pixelShow();
-	  HAL_Delay(50);
+      HAL_ADC_Start_DMA(&hadc1, (uint32_t*)lecturas_sensores, 4);
+      HAL_Delay(20);                     // wait for ~4 conversions to finish
+      HAL_ADC_Stop_DMA(&hadc1);
+
+      for(int i = 0; i < 4; i++) {
+          if(lecturas_sensores[i] < 230) {
+              // Valor menor a 230 -> Color VERDE (DISPONIBLE)
+              setPixelColor(i, 0, 255, 0);
+              aTxBuffer[i] = 0; // Se envía al ESP32 que está libre
+          } else {
+              // Valor mayor o igual a 230 -> Color ROJO (OCUPADO)
+              setPixelColor(i, 255, 0, 0);
+              aTxBuffer[i] = 1; // Se envía al ESP32 que está ocupado
+          }
+      }
+      pixelShow();
+      HAL_Delay(50);
   }
   /* USER CODE END 3 */
 }
@@ -189,16 +204,7 @@ void SystemClock_Config(void)
   */
 static void MX_ADC1_Init(void)
 {
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
   ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
 
   /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
   */
@@ -255,10 +261,29 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
+}
 
-  /* USER CODE END ADC1_Init 2 */
-
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  // 0x18 << 1 = 0x30
+  hi2c1.Init.OwnAddress1 = 0x30;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
@@ -268,19 +293,11 @@ static void MX_ADC1_Init(void)
   */
 static void MX_TIM1_Init(void)
 {
-
-  /* USER CODE BEGIN TIM1_Init 0 */
-
-  /* USER CODE END TIM1_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -329,11 +346,7 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
-
 }
 
 /**
@@ -343,14 +356,6 @@ static void MX_TIM1_Init(void)
   */
 static void MX_USART2_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -363,10 +368,6 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
@@ -374,7 +375,6 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_DMA_Init(void)
 {
-
   /* DMA controller clock enable */
   __HAL_RCC_DMA2_CLK_ENABLE();
 
@@ -385,7 +385,6 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream1_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
-
 }
 
 /**
@@ -396,9 +395,6 @@ static void MX_DMA_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -441,14 +437,36 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c){
+    HAL_I2C_EnableListen_IT(hi2c);
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode){
+    if(TransferDirection == I2C_DIRECTION_TRANSMIT){
+        // El Maestro (ESP32) quiere transmitir, la STM32 debe RECIBIR
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c, (uint8_t *) aRxBuffer, 1, I2C_FIRST_AND_LAST_FRAME);
+    } else if (TransferDirection == I2C_DIRECTION_RECEIVE){
+        // El Maestro (ESP32) quiere recibir, la STM32 debe TRANSMITIR
+        HAL_I2C_Slave_Seq_Transmit_IT(hi2c, (uint8_t *) aTxBuffer, 4, I2C_FIRST_AND_LAST_FRAME);
+    }
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c){
+    if (aRxBuffer[0] == 'S'){
+        // Código opcional si necesitas reaccionar al comando S
+    }
+    aRxBuffer[0] = 0x00;
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c){
+    // Quitamos la trampa del Error_Handler.
+    // Ahora, si hay un fallo de comunicación, simplemente reactiva la escucha.
+    HAL_I2C_EnableListen_IT(hi2c);
+}
 /* USER CODE END 4 */
 
 /**
@@ -465,10 +483,11 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-#ifdef USE_FULL_ASSERT
+
+#ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
+  * where the assert_param error has occurred.
   * @param  file: pointer to the source file name
   * @param  line: assert_param error line source number
   * @retval None

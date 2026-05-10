8129c2907a42467c22579b7a7a556b723a08382d
Simon <dua23615@uvg.edu.gt>
2026-05-09 23:52:00 -0600

    I2C works

--- STM_TFT/Core/Src/main.c
+++ STM_TFT/Core/Src/main.c
@@ -76,6 +76,8 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi1;
 
 TIM_HandleTypeDef htim1;
@@ -90,6 +92,11 @@ float brilloled = 50.0f;
 static uint8_t spots_now[NUM_SPOTS]    = {0};
 static uint8_t spots_drawn[NUM_SPOTS]  = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 static uint8_t avail_drawn = 0xFF;
+
+// Buffers para I2C
+uint8_t aTxBuffer[4] = {0, 0, 0, 0}; // Estado de los 4 parqueos (0=Libre, 1=Ocupado)
+uint8_t aRxBuffer[1] = {0};          // Para recibir el comando 'S' del ESP32
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -100,6 +107,7 @@ static void MX_TIM1_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -249,17 +257,22 @@ int main(void)
   MX_USART2_UART_Init();
   MX_SPI1_Init();
   MX_ADC1_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   HAL_ADC_Start_DMA(&hadc1, (uint32_t*)lecturas_sensores, 4);
+
   setBrightness(25);
   pixelClear();
   pixelShow();
-  HAL_Delay(1000);
+  //HAL_Delay(1000);
   LCD_Init();
   LCD_Clear(0x10a2);
   LCD_Print("Parqueo-matic", 100, 6, 1, 0xFFFF, 0x10a2);
 
+  HAL_I2C_EnableListen_IT(&hi2c1);
+
   Parking_DrawStaticUI();
+  //HAL_Delay(1000);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -273,9 +286,11 @@ int main(void)
 			if(lecturas_sensores[i] < 230) {
 				// Valor menor a 230 -> Color VERDE
 				setPixelColor(i, 0, 255, 0);
+				aTxBuffer[i] = 0; // Se envía al ESP32 que está libre
 			} else {
 				// Valor mayor o igual a 230 -> Color ROJO
 				setPixelColor(i, 255, 0, 0);
+				aTxBuffer[i] = 1; // Se envía al ESP32 que está ocupado
 			}
 		}
 	  pixelShow();
@@ -416,6 +431,40 @@ static void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 78;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -659,6 +708,33 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c){
+    HAL_I2C_EnableListen_IT(hi2c);
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode){
+    if(TransferDirection == I2C_DIRECTION_TRANSMIT){
+        // El Maestro (ESP32) quiere transmitir, la STM32 debe RECIBIR
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c, (uint8_t *) aRxBuffer, 1, I2C_FIRST_AND_LAST_FRAME);
+    } else if (TransferDirection == I2C_DIRECTION_RECEIVE){
+        // El Maestro (ESP32) quiere recibir, la STM32 debe TRANSMITIR
+        HAL_I2C_Slave_Seq_Transmit_IT(hi2c, (uint8_t *) aTxBuffer, 4, I2C_FIRST_AND_LAST_FRAME);
+    }
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c){
+    if (aRxBuffer[0] == 'S'){
+        // Código opcional si necesitas reaccionar al comando S
+    }
+    aRxBuffer[0] = 0x00;
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c){
+    // Quitamos la trampa del Error_Handler.
+    // Ahora, si hay un fallo de comunicación, simplemente reactiva la escucha.
+    HAL_I2C_EnableListen_IT(hi2c);
+}
 
 /* USER CODE END 4 */
 
